13ce58f888279c1ed93cda02d1d56e9c7d5a1f74
waystedd <160508650+waystedd@users.noreply.github.com>
2026-04-08 01:03:26 +0300

    Update app.py
    Changed the spacing

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from routes.recipes import recipes_bp
 from config import Config
 
+
 def create_app():
     app = Flask(__name__, static_folder='../frontend', static_url_path='')
     app.config.from_object(Config)
@@ -19,6 +20,7 @@ def create_app():
         return {"status": "ok"}, 200
 
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
